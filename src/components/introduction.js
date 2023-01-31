2228a2b71fb435bcc2415d6d9c9c6452395e6882
MajaSchermuly <maja9756@tamu.edu>
2023-01-31 00:06:46 -0600

    intro and heirarchy material

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -18,10 +18,12 @@ function Introduction() {
             <Col className="center">
                 <h5>Howdy</h5>
                 <br/>
-                <p>I'm a senior computer science major at Texas A&M University.</p>
+                <p>I'm a senior at Texas A&M University pursuing my Computer Science Degree with a minor in business.</p>
+                <br/>
+                <p>My interests in computers and finance have led me to focus on full stack application development in the realm of financial technology. Adjacent to this I have gained experience in computer graphics, cloud engineering, and deeper analysis of data structures and algorithms. </p>
                 <br/>
                 <p>I'm taking Computers and New Media to familiarize myself with the patterns of changing media and how today's media access impacts society. </p>
-
+                <br/>
             </Col>
             <Col></Col>
         </Row>
